be8bd23a4c7e50ee94cd985cafb9d3550c7038dd
Bre <bloombaby6789@gmail.com>
2016-11-09 20:17:57 -0600

    Set up new profile view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :relationships
   has_many :friends, through: :relationships
+
+  has_many :inverse_relationships, class_name: "Relationship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_relationships, source: :user
 end
